cd746f2c4c834d204febdcac56ac2c04d5585701
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-11-02 19:31:49 -0500

    new stuff

---
+++ NODE-JS/10-3/10-3-2/js/index.js
@@ -0,0 +1,8 @@
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+
+const myFather = new Person("Moe", "Doe", 50, "blue");
